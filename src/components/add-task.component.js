d97d7659918ca5ee7408f09c0cb6d495dc30efeb
Calvin Kinateder <calvinkinateder1@yahoo.com>
2021-03-21 15:25:29 -0400

    Create primitive front end; begin detailing next

---
+++ src/components/add-task.component.js
@@ -0,0 +1,159 @@
+import React, { Component } from "react";
+import TaskDataService from "../services/task.service";
+
+export default class AddTask extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeType = this.onChangeType.bind(this);
+    this.onChangePriority = this.onChangePriority.bind(this);
+    this.saveTask = this.saveTask.bind(this);
+    this.newTask = this.newTask.bind(this);
+
+    this.state = {
+      id: null,
+      title: "",
+      description: "", 
+      type: "to do",
+      priority: "low",
+
+      submitted: false
+    };
+  }
+
+  onChangeTitle(e) {
+    this.setState({
+      title: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangeType(e) {
+    this.setState({
+      type: e.target.value
+    });
+  }
+
+  onChangePriority(e) {
+    this.setState({
+      priority: e.target.value
+    });
+  }
+
+  saveTask() {
+    var data = {
+      title: this.state.title,
+      description: this.state.description,
+      type: this.state.type,
+      priority: this.state.priority
+    };
+
+    TaskDataService.create(data)
+      .then(response => {
+        this.setState({
+          id: response.data.id,
+          title: response.data.title,
+          description: response.data.description,
+          type: response.data.type,
+          priority: response.data.priority,
+
+          submitted: true
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  newTask() {
+    this.setState({
+      id: null,
+      title: "",
+      description: "",
+      type: "to do",
+      priority: "low",
+
+      submitted: false
+    });
+  }
+
+  render() {
+    return (
+      <div className="submit-form">
+        {this.state.submitted ? (
+          <div>
+            <h4>You submitted successfully!</h4>
+            <button className="btn btn-success" onClick={this.newTask}>
+              Add
+            </button>
+          </div>
+        ) : (
+          <div>
+            <div className="form-group">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                className="form-control"
+                id="title"
+                required
+                value={this.state.title}
+                onChange={this.onChangeTitle}
+                name="title"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                className="form-control"
+                id="description"
+                required
+                value={this.state.description}
+                onChange={this.onChangeDescription}
+                name="description"
+              />
+            </div>
+  
+            <div className="form-group">
+            <label htmlFor="description">type</label>
+            <input
+                type="text"
+                className="form-control"
+                id="type"
+                required
+                value={this.state.type}
+                onChange={this.onChangeType}
+                name="type"
+            />
+            </div>
+  
+            <div className="form-group">
+            <label htmlFor="description">priority</label>
+            <input
+                type="text"
+                className="form-control"
+                id="priority"
+                required
+                value={this.state.priority}
+                onChange={this.onChangePriority}
+                name="priority"
+            />
+            </div>
+
+            <button onClick={this.saveTask} className="btn btn-success">
+              Submit
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
---
+++ src/components/task.component.js
@@ -0,0 +1,228 @@
+import React, { Component } from "react";
+import TaskDataService from "../services/task.service";
+
+export default class Task extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeType = this.onChangeType.bind(this);
+    this.onChangePriority = this.onChangePriority.bind(this);
+    this.getTask = this.getTask.bind(this);
+    //this.updatePublished = this.updatePublished.bind(this);
+    this.updateTask = this.updateTask.bind(this);
+    this.deleteTask = this.deleteTask.bind(this);
+
+    this.state = {
+      currentTask: {
+        id: null,
+        title: "",
+        description: "",
+        type: "to do",
+        priority: "in progress",
+      },
+      message: ""
+    };
+  }
+
+  componentDidMount() {
+    this.getTask(this.props.match.params.id);
+  }
+
+  onChangeTitle(e) {
+    const title = e.target.value;
+
+    this.setState(function(prevState) {
+      return {
+        currentTask: {
+          ...prevState.currentTask,
+          title: title
+        }
+      };
+    });
+  }
+
+  onChangeDescription(e) {
+    const description = e.target.value;
+    
+    this.setState(prevState => ({
+      currentTask: {
+        ...prevState.currentTask,
+        description: description
+      }
+    }));
+  }
+
+  onChangeType(e) {
+    const type = e.target.value;
+    
+    this.setState(prevState => ({
+      currentTask: {
+        ...prevState.currentTask,
+        type: type
+      }
+    }));
+  }
+
+  onChangePriority(e) {
+    const priority = e.target.value;
+    
+    this.setState(prevState => ({
+      currentTask: {
+        ...prevState.currentTask,
+        priority: priority
+      }
+    }));
+  }
+
+  getTask(id) {
+    TaskDataService.get(id)
+      .then(response => {
+        this.setState({
+          currentTask: response.data
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+/** 
+  updatePublished(status) {
+    var data = {
+      id: this.state.currentTask.id,
+      title: this.state.currentTask.title,
+      description: this.state.currentTask.description,
+      published: status
+    };
+
+    TaskDataService.update(this.state.currentTask.id, data)
+      .then(response => {
+        this.setState(prevState => ({
+          currentTask: {
+            ...prevState.currentTask,
+            published: status
+          }
+        }));
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }*/
+
+  updateTask() {
+    TaskDataService.update(
+      this.state.currentTask.id,
+      this.state.currentTask
+    )
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          message: "The task was updated successfully!"
+        });
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  deleteTask() {    
+    TaskDataService.delete(this.state.currentTask.id)
+      .then(response => {
+        console.log(response.data);
+        this.props.history.push('/tasks')
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  render() {
+    const { currentTask } = this.state;
+
+    return (
+      <div>
+        {currentTask ? (
+          <div className="edit-form">
+            <h4>Task</h4>
+            <form>
+              <div className="form-group">
+                <label htmlFor="title">Title</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="title"
+                  value={currentTask.title}
+                  onChange={this.onChangeTitle}
+                />
+              </div>
+              
+              <div className="form-group">
+                <label htmlFor="description">Description</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="description"
+                  value={currentTask.description}
+                  onChange={this.onChangeDescription}
+                />
+              </div>
+              
+              <div className="form-group">
+                <label htmlFor="type">type</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="type"
+                  value={currentTask.type}
+                  onChange={this.onChangeType}
+                />
+              </div>
+              
+              <div className="form-group">
+                <label htmlFor="priority">priority</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="priority"
+                  value={currentTask.priority}
+                  onChange={this.onChangePriority}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>
+                  <strong>Type:</strong>
+                </label>
+                {currentTask.type}
+              </div>
+            </form>
+            
+
+            <button
+              className="badge badge-danger mr-2"
+              onClick={this.deleteTask}
+            >
+              Delete
+            </button>
+
+            <button
+              type="submit"
+              className="badge badge-success"
+              onClick={this.updateTask}
+            >
+              Update
+            </button>
+            <p>{this.state.message}</p>
+          </div>
+        ) : (
+          <div>
+            <br />
+            <p>Please click on a Task...</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
